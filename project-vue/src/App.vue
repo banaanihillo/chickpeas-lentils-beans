4bf293015b88b739d926daf79ada4798994f4af6
banaanihillo <banaanihillo@protonmail.ch>
2020-06-27 15:48:40 +0300

    Introducing myself into the Vue build process
    Starting to figure out how to actually develop things,
    also added a component with props
    in the form of a data property bound onto the component

--- project-vue/src/App.vue
+++ project-vue/src/App.vue
@@ -1,28 +1,49 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id = "app">
+        <hello-everyone-else :greetees = "properties.greetees" />
+        <img alt = "Vue logo" src = "./assets/logo.png">
+        <HelloWorld message = "Hello!" />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    import HelloWorld from "./components/HelloWorld.vue"
+    import HelloEveryoneElse from "./components/HelloEveryoneElse.vue"
+    
+    export default {
+        name: "App",
+        components: {
+            HelloWorld,
+            HelloEveryoneElse
+        },
+        data() {
+            return {
+                properties: {
+                    greetees: [
+                        {
+                            name: "J Doe",
+                            favoriteThing: "Just hangin around",
+                            id: (Math.ceil(Math.random() * 1000000))
+                        },
+                        {
+                            name: "Joe D",
+                            favoriteThing: "Bananas",
+                            id: (Math.floor(Math.random() * 222222))
+                        }
+                    ]
+                }
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ project-vue/src/components/HelloEveryoneElse.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <ul v-for = "greetee in greetees" v-bind:key = "greetee.key">
+            Hello, {{greetee.name}}!
+            So, {{greetee.favoriteThing}} is your favorite, eh?
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HelloEveryoneElse",
+        props: {
+            greetees: {
+                type: Array,
+                required: true
+            }
+        }
+    }
+</script>
