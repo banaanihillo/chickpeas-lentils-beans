342d246c5a203e8498efa1e5c7f87e92fe167239
banaanihillo <banaanihillo@protonmail.ch>
2020-07-14 12:22:47 +0300

    Added support for the score slashline and extra innings
    Figuring out the most effective way for the boxscore display,
    especially whenever the browser window is resized,
    also figured out how to add new implicit columns,
    to properly insert extra innings whenever necessary

--- play-ball/src/components/BoxScore.vue
+++ play-ball/src/components/BoxScore.vue
@@ -2,22 +2,40 @@
     <div>
         <span id = "boxscore">
             <span v-for = "inning in innings" v-bind:key = "inning">
-                {{inning}} <br /> 
+                {{inning}} <br />
+                <input type = "number" v-bind:value = 0 /> <br />
+                <input type = "number" v-bind:value = 0 /> <br />
             </span>
-                    
+            <span>
+                R <br />
+                <input type = "number" v-bind:value = awayTeamRuns /> <br />
+                <input type = "number" v-bind:value = homeTeamRuns /> <br />
+            </span>
+            <span>
+                H <br />
+                <input type = "number" v-bind:value = 0 /> <br />
+                <input type = "number" v-bind:value = 0 /> <br />
+            </span>
+            <span>
+                E <br />
+                <input type = "number" v-bind:value = 0 /> <br />
+                <input type = "number" v-bind:value = 0 /> <br />
+            </span>
+            <!--        
             <span v-for = "inning in innings" v-bind:key = "'Top' + inning">
                 <input type = "number" v-bind:value = 0 />
             </span>
             <span v-for = "inning in innings" v-bind:key = "'Bottom' + inning">
                 <input type = "number" v-bind:value = 0 />
             </span>
+            -->
         </span>
 
-        <!--
+
         <button @click.prevent = "innings++">
             Extra innings
         </button>
-        -->
+        
     </div>
 </template>
 
@@ -26,7 +44,9 @@
         name: "BoxScore",
         data() {
             return {
-                innings: 9
+                innings: 9,
+                awayTeamRuns: 0,
+                homeTeamRuns: 0
             }
         }
     }
@@ -39,12 +59,15 @@
         margin-top: 10px;
         display: grid;
         grid-template-columns: repeat(9, 5ch);
+        grid-auto-columns: 5ch;
+        grid-auto-rows: 1fr;
         justify-content: center;
-        text-align: center;
+        grid-auto-flow: column;
     }
     input {
         width: 3ch;
         height: 1fr;
         font-size: 3ch;
+        text-align: center;
     }
 </style>
