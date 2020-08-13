916d0f603555d5a215b7584ffc49fbb98bb00d7a
banaanihillo <banaanihillo@protonmail.ch>
2020-08-13 17:49:26 +0300

    Adding stuff required for deployment

--- node_stuff/index.js
+++ node_stuff/index.js
@@ -3,7 +3,7 @@ const http = require("http")
 
 const server = http.createServer(app)
 
-const port = 9001
+const port = process.env.PORT || 9001
 server.listen(port, () => {
     console.log(`Server running on the port ${port}.`)
 })
