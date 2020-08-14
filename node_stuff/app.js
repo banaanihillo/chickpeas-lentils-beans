510b997c3e045d1ebd48b4f96e0f44a6d5da5da6
banaanihillo <banaanihillo@protonmail.ch>
2020-08-14 13:24:31 +0300

    Deleting and de-deleting things

---
+++ node_stuff/app.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const mongoose = require("mongoose")
+require("dotenv").config()
+app.use(cors())
+app.use(express.json())
+
+const baseURL = process.env.MONGODB_URI
+const connectToMongo = () => {
+    mongoose.connect(baseURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    })
+}
+try {
+    connectToMongo()
+    console.log("Successfully connected to Mongo.")
+} catch (error) {
+    console.log("Could not connect to Mongo for some reason.")
+    console.log(error)
+}
+
+const playerRouter = require("./controllers/playerRouter")
+app.use("/players", playerRouter)
+const teamRouter = require("./controllers/teamRouter")
+app.use("/teams", teamRouter)
+
+module.exports = app
--- node_stuff/index.js
+++ node_stuff/index.js
@@ -1,35 +1,9 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const mongoose = require("mongoose")
-require("dotenv").config()
-app.use(cors())
-app.use(express.json())
-app.use(express.static("dist"))
+const app = require("./app")
+const http = require("http")
 
-const baseURL = process.env.MONGODB_URI
-const connectToMongo = () => {
-    mongoose.connect(baseURL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true
-    })
-}
-try {
-    connectToMongo()
-    console.log("Successfully connected to Mongo.")
-} catch (error) {
-    console.log("Could not connect to Mongo for some reason.")
-    console.log(error)
-}
-
-const playerRouter = require("./controllers/playerRouter")
-app.use("/players", playerRouter)
-const teamRouter = require("./controllers/teamRouter")
-app.use("/teams", teamRouter)
+const server = http.createServer(app)
 
 const PORT = process.env.PORT || 9001
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server running on the port ${PORT}.`)
 })
