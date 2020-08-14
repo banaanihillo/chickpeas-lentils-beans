5d9d990743fe4df36da7326376498f500a211bcd
banaanihillo <banaanihillo@protonmail.ch>
2020-08-14 13:06:19 +0300

    Six four three

--- node_stuff/index.js
+++ node_stuff/index.js
@@ -1,9 +1,35 @@
-const app = require("./app")
-const http = require("http")
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const mongoose = require("mongoose")
+require("dotenv").config()
+app.use(cors())
+app.use(express.json())
+app.use(express.static("dist"))
 
-const server = http.createServer(app)
+const baseURL = process.env.MONGODB_URI
+const connectToMongo = () => {
+    mongoose.connect(baseURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    })
+}
+try {
+    connectToMongo()
+    console.log("Successfully connected to Mongo.")
+} catch (error) {
+    console.log("Could not connect to Mongo for some reason.")
+    console.log(error)
+}
 
-const port = process.env.PORT || 9001
-server.listen(port, () => {
-    console.log(`Server running on the port ${port}.`)
+const playerRouter = require("./controllers/playerRouter")
+app.use("/players", playerRouter)
+const teamRouter = require("./controllers/teamRouter")
+app.use("/teams", teamRouter)
+
+const PORT = process.env.PORT || 9001
+app.listen(PORT, () => {
+    console.log(`Server running on the port ${PORT}.`)
 })
