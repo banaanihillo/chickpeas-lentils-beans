ca2ac3063dafa2d7bad2bf479f8be533951b6126
banaanihillo <banaanihillo@protonmail.ch>
2021-05-12 12:24:38 +0300

    Fixed the history API fallback(?)
    Figuring out why health check works,
    but navigation directly does not,
    also trying to fix "cannot GET /players" etc

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const app = require("./app")
 const http = require("http")
+const history = require("connect-history-api-fallback")
+app.use(history)
 
 const server = http.createServer(app)
 
 const PORT = process.env.PORT || 9001
 server.listen(PORT, () => {
     console.log(`Server running on the port ${PORT}.`)
+})
+
+server.get("/healthcheck", (_request, response) => {
+  response.send("OK")
 })
